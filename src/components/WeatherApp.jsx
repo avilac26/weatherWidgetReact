03ebb8c694b41816a670031ccd3094db928e1a57
avilac26 <emanuelavilac@gmail.com>
2025-12-23 20:24:05 -0600

    .env file location corrected. City input created getting temperature from API

--- src/components/WeatherApp.jsx
+++ src/components/WeatherApp.jsx
@@ -1,11 +1,42 @@
+import { useEffect, useState } from "react";
+import WeatherForm from "./WeatherForm";
 
 
 function WeatherApp(){
+    const [weather, setWeather]=useState(null);
 
+    
+    
+    async function loadInfo(city = "london"){
+        try {
+            const request = await fetch(`${import.meta.env.VITE_WEATHER_URL}key=${import.meta.env.VITE_WEATHER_KEY}&q=${encodeURIComponent(city)}`
+            );
+
+            const json = await request.json();
+            console.log("API RESPONSE:", json);
+
+            if (json.error) {
+                console.error("API ERROR:", json.error.message);
+                return;
+            }
+
+            setWeather(json);
+
+        } catch (error){
+            console.error("FETCH ERROR:", error);
+        }
+
+    }
+
+    function handleOnchangeCity(city){
+        setWeather(null);
+        loadInfo(city);
+    }
 
     return(
         <div>
-            <h1>Hola</h1>
+            <WeatherForm onChangeCity={handleOnchangeCity} />
+            <div>{weather?.current.temp_c}</div>
         </div>
     )
 }
